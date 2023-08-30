3fe572f79f074e107448a95a3aa7b24c24da1e05
delfinaronco <142460374+delfinaronco@users.noreply.github.com>
2023-08-30 20:24:19 -0300

    terminado

--- TEST.hs
+++ TEST.hs
@@ -24,6 +24,67 @@ absoluto :: Int -> Int
 absoluto x | x >= 0 = x
            | otherwise = (-x)
 
+ --b)
+maximoabsoluto :: Int -> Int -> Int       
+maximoabsoluto x y | (absoluto x) >= (absoluto y) = x
+                   | otherwise = y
+
+--c)
+maximo3 :: Int -> Int -> Int -> Int
+maximo3 x y z | x >= y && x >= z = x
+              | y >= x && y >= z = y
+              | otherwise =z
+
+--d)
+--sin usar pattern matching
+algunoEsO :: Float -> Float -> Bool
+algunoEsO x y | x==0 || y==0 = True
+              | otherwise = False
+
+--usando pattern matching
+algunoEsOp :: Float -> Float -> Bool
+algunoEs0p 0 y = True
+algunoEsOp x 0 = True
+algunoEsOp x y = False
+
+--e)
+--sin usar pattern matching
+ambosSon0 :: Float -> Float -> Bool
+ambosSon0 x y | x==0 && y==0 = True
+              | otherwise = False
+
+--usando pattern matching
+ambosSon0p :: Float -> Float -> Bool
+ambosSon0p 0 0 = True
+ambosSon0p x y = False
+
+--f)
+mismoIntervalo :: Float -> Float -> Bool
+mismoIntervalo x y | x <= 3 && y <= 3 = True
+                   | (x > 3 && x < 7) && (y > 3 && y < 7) = True
+                   | x > 7 && y > 7 = True
+                   | otherwise = False
+
+--g)
+sumaDistintos :: Int -> Int -> Int -> Int
+sumaDistintos x y z | (x == y) && (x == z) = x
+                    | x == y = x + z
+                    | x == z = x + y
+                    | otherwise = x + y + z
+
+--h)
+esMultiploDe :: Int -> Int -> Bool
+esMultiploDe x y | mod x y == 0 = True
+                 | otherwise = False
+
+--i)
+digitoUnidades :: Int -> Int
+digitoUnidades x = mod x 10
+
+--j)
+digitoDecenas :: Int -> Int
+digitoDecenas x = mod (div x 10) 10
+
 --Ejercicio 3
 estanRelacionados :: Int -> Int -> Bool
 estanRelacionados a b | a == 0 && b == 0 = False
@@ -104,72 +165,7 @@ comparar a b | sumaUltimosDosDigitos a < sumaUltimosDosDigitos b = 1
               where sumaUltimosDosDigitos x = mod x 10 + mod (div x 10) 10
 
 
- --b)
-maximoabsoluto :: Int -> Int -> Int       
-maximoabsoluto x y | (absoluto x) >= (absoluto y) = x
-                   | otherwise = y
 
---c)
-maximo3 :: Int -> Int -> Int -> Int
-maximo3 x y z | x >= y && x >= z = x
-              | y >= x && y >= z = y
-              | otherwise =z
-
---d)
---sin usar pattern matching
-algunoEsO :: Float -> Float -> Bool
-algunoEsO x y | x==0 || y==0 = True
-              | otherwise = False
-
---usando pattern matching
-algunoEsOp :: Float -> Float -> Bool
-algunoEs0p 0 y = True
-algunoEsOp x 0 = True
-algunoEsOp x y = False
-
---e)
---sin usar pattern matching
-ambosSon0 :: Float -> Float -> Bool
-ambosSon0 x y | x==0 && y==0 = True
-              | otherwise = False
-
---usando pattern matching
-ambosSon0p :: Float -> Float -> Bool
-ambosSon0p 0 0 = True
-ambosSon0p x y = False
-
---f)
-mismoIntervalo :: Float -> Float -> Bool
-mismoIntervalo x y | x <= 3 && y <= 3 = True
-                   | (x > 3 && x < 7) && (y > 3 && y < 7) = True
-                   | x > 7 && y > 7 = True
-                   | otherwise = False
-
---g)
-sumaDistintos :: Int -> Int -> Int -> Int
-sumaDistintos x y z | (x == y) && (x == z) = x
-                    | x == y = x + z
-                    | x == z = x + y
-                    | otherwise = x + y + z
-
---h)
-esMultiploDe :: Int -> Int -> Bool
-esMultiploDe x y | mod x y == 0 = True
-                 | otherwise = False
-
---i)
-digitoUnidades :: Int -> Int
-digitoUnidades x = mod x 10
-
---j)
-digitoDecenas :: Int -> Int
-digitoDecenas x = mod (div x 10) 10
-
---Ejercicio 3
-estanRelacionados :: Int -> Int -> Bool
-estanRelacionados a b | a == 0 && b == 0 = False
-                      | mod a b == 0 = True 
-                      | otherwise = False
 
 
 
