da909b23b1791ca4cbe3347843aef9f854eb791b
delfinaronco <142460374+delfinaronco@users.noreply.github.com>
2023-10-03 20:13:06 -0300

    Update Guía6.py

--- Guía6.py
+++ Guía6.py
@@ -22,7 +22,6 @@ def factorial_de_dos() -> int:
 def perimetro () -> float:
 	return 2 * math.pi
 
-
 # Ejercicio 2
 
 # 1)
@@ -31,27 +30,19 @@ def imprimir_saludo (name:str) -> str:
 	res: str = f"Hola {name}"
 	print (res)
 
-# imprimir_saludo("delfina")
-
 # 2) 
 def raiz_cuadrada_de (n:int):
 	return math.sqrt(n)
-
-# print(raiz_cuadrada_de(49))
 
 # 3) 
 def farenheit_a_celsius (t:float) -> float:
     res = round (((t - 32) * 5)/9)
     print(res)
 
-# farenheit_a_celsius(75)
-
 # 4)
 def imprimir_dos_veces(e:str) -> str:
 	print(e)
 	print(e)
-
-# imprimir_dos_veces("my name's Blurryface and I care what you think")
 
 # 5)
 def es_multiplo_de (n:int, m:int) -> bool:
@@ -64,29 +55,47 @@ def es_par(n:int) -> bool:
 	else: 
 		return False
 
-# print(es_par(4))
-
 # 7)
-def cantidad_de_pizzas(comensales:int,min_cant_porciones:int) -> int:
+def cantidad_de_pizzas(comensales:int, min_cant_porciones:int) -> int:
 	if (comensales * min_cant_porciones) <= 8: 
 		return 1
 	else:
-		return (((comensales * min_cant_porciones) // 8) + 1)
-
-# print (cantidad_de_pizzas(2,8))
-
+		return ((math.ceil((comensales * min_cant_porciones) // 8)) + 1)
 
 # Ejercicio 3
+
+# 1)
+def alguno_es_0 (n1:float, n2:float) -> bool:
+	return n1 == 0 or n2 == 0 
+
+# 2)
+def ambos_son_0 (n1:float, n2:float) -> bool:
+	return n1 == 0 and n2 == 0
 
 # 3)
 def es_nombre_largo (s:str) -> bool:
 	return len (s) >= 3 and len (s) <= 8
 
-"""
-print (es_nombre_largo ("ab"))
-print (es_nombre_largo ("sdgfysgv"))
+# 4)
+def es_bisiesto (año: int) -> bool:
+    return es_multiplo_de (año,4) and not (es_multiplo_de (año,100))
 
-"""
+# Ejercicio 4
+
+# 1)
+def peso_pino (metros:int) -> int:
+	if metros <= 3:
+		return 3 * metros * 100
+	else: 
+		return 900 + (2 * (metros - 3) * 100)
+
+# 2)
+def es_peso_util (peso_pino) -> bool:
+	return peso_pino >= 400 and peso_pino <= 1000
+
+# 4)
+def sirve_pino2 (metros: int) -> bool:
+	return peso_pino (metros) >= 400 and peso_pino (metros) <= 1000
 
 # Ejercicio 5
 
@@ -99,13 +108,63 @@ def devolver_el_doble_si_es_par (n:int) -> int:
 
 	return res  
 
-"""
-print (devolver_el_doble_si_es_par(5))
-print (devolver_el_doble_si_es_par(4))
+# 2)
+def devolver_valor_si_es_par_sino_el_que_sigue (n: int) -> int:
+	if es_par (n): 
+		res = n
+	else: 
+		res = n + 1
+	return res
 
-"""
+# 3) 
+def devolver_el_doble_si_es_multiplo3_el_triple_si_es_multiplo9 (n: int) -> int:
+	if es_multiplo_de (n,3) and not(es_multiplo_de (n,9)):
+		res = n * 2
+	elif es_multiplo_de (n,9):
+		res = n * 3
+	else: 
+		res = n
+	return res
 
-# Ejercicio 6
+# 4)
+def lindo_nombre (nombre: str) -> str:
+	if len (nombre) >= 5:
+		print ("Tu nombre tiene muchas letras")
+	else:
+		print ("Tu nombre tiene menos de 5 caracteres")
+
+# 5)
+def el_rango (n: int) -> str:
+	if n < 5:
+		print ("Menor a 5")
+	elif n >= 10 and n <= 20:
+		print ("Entre 10 y 20")
+	elif n > 20:
+		print ("Mayor a 20")
+
+# 6)
+def vacaciones_o_trabajar (sexo: str, edad: int) -> str:
+	if sexo == "F" and (edad < 18 or edad >= 60):
+		print ("Andá de vacaciones")
+	elif sexo == "F" and (edad >= 18 or edad < 60):
+		print ("Te toca trabajar")
+	elif sexo == "M" and (edad < 18 or edad >= 65):
+		print ("Andá de vacaciones")
+	elif sexo == "M" and (edad >= 18 or edad < 65):
+		print ("Te toca trabajar") 
+	
+# Ejercicio 6/7
+
+# 1)
+def imprime_1_a_10 ():
+	n: int = 1
+	while n <= 10:
+		print(n)
+		n = n + 1
+
+def imprime_1_a_10_for ():
+	for n in range (1,11,1):
+		print(n)
 
 # 2)
 def imprime_pares_10_40():
@@ -114,14 +173,23 @@ def imprime_pares_10_40():
 		print(n)
 		n = n + 2
 
-
-def imprime_pares_10_40_for():   # hace lo mismo que el anterior
+def imprime_pares_10_40_for():  
 	for n in range(10,42,2):
 		print(n)
 
-# 4)
-# varias maneras
+# 3)
+def eco_10_veces():
+	n = 1
+	while n <= 10:
+		print ("eco")
+		n = n + 1
+	
+def eco_10_veces_for():
+	n = 1
+	for n in range(n,11):
+		print("eco")
 
+# 4)
 def cuenta_regresiva(n:int):
 	while n > 0:
 		print (n)
@@ -137,3 +205,22 @@ def cuenta_regresiva_for2(n:int):
 	for i in range (n,0,-1):
 		print(i)
 	print ("Despegue")
+
+# 5)
+def viaje_en_el_tiempo(partida: int, llegada: int):
+    while partida != llegada:
+        partida = partida - 1
+        print("Viajó un año al pasado, estamos en el año:", partida)
+
+def viaje_en_el_tiempo_for (partida: int, llegada: int):
+	for i in range (partida - 1, llegada - 1, -1):
+		print ("Viajó un año al pasado, estamos en el año:",i)
+
+# 6)
+def viaje_aristoteles(año_partida: int)-> str:
+        año_aristoteles = -384
+        i = 20
+        while año_partida > año_aristoteles:
+            print(f"viajaste {i} años en el tiempo, estamos en el año {año_partida-20}")
+            año_partida -= 20
+            i += 20
