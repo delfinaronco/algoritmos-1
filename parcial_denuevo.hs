daee6ab770b4efde2f71e7aed71026e3388e62e3
delfinaronco <142460374+delfinaronco@users.noreply.github.com>
2023-11-16 16:03:55 -0300

    Update parcial_denuevo.hs

--- parcial_denuevo.hs
+++ parcial_denuevo.hs
@@ -21,9 +21,23 @@ votosEnBlanco formulas votos cantTotalVotos = cantTotalVotos - (sumatoria votos)
 
 -- Ejercicio 3
 
---porcentajeDeVotos :: String -> [(String,String)] -> [Int] -> Float
---porcentajeDeVotos presidente formulas votos 
+porcentajeDeVotos :: String -> [(String,String)] -> [Int] -> Float
+porcentajeDeVotos presidente formulas votos = (division (votosDelPresidente presidente formulas votos) (sumatoria votos)) * 100
 
 votosDelPresidente :: String -> [(String,String)] -> [Int] -> Int
-votosDelPresidente presidente ((presi,vice):xs) votos | presidente == presi = votos[0]
-                                                      | otherwise = votosDelPresidente presidente xs votos
+votosDelPresidente presidente ((presi,vice):xs) (y:ys) | presidente == presi = y
+                                                       | otherwise = votosDelPresidente presidente xs ys
+
+division :: Int -> Int -> Float
+division a b = (fromIntegral a) / (fromIntegral b)
+
+-- Ejercicio 4 
+
+proximoPresidente :: [(String,String)] -> [Int] -> String
+proximoPresidente ((presi,vice):xs) (y:ys) | maximo (y:ys) == y = presi
+                                           | otherwise = proximoPresidente xs ys
+
+maximo :: [Int] -> Int
+maximo [x] = x
+maximo (x:y:xs) | x >= y = maximo (x:xs)
+                | otherwise = maximo (y:xs)
